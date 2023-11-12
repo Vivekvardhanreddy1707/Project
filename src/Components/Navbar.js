905e2db83168b283dae74d0f475941dc008c766a
B Vivek Vardhan Reddy <144028123+Vivekvardhanreddy1707@users.noreply.github.com>
2023-11-12 21:37:15 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,8 @@
-import React from 'react';
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from 'react';
 
-function Navbar() {
-  const scrollToSection = (sectionId) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
+const Navbar = ({ scrollToSection, handleSignupClick }) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-0">
+    <nav id="navbar" className="navbar navbar-expand-lg navbar-dark bg-dark p-0">
       <div className="container-fluid p-0">
         <a className="navbar-brand" href="#">
           <h3 className="navbar-text text-white m-0 p-0">Plates&Pleasure</h3>
@@ -26,8 +18,8 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-between p-0" id="navbarNav">
-        <ul className="navbar-nav">
+        <div className="collapse navbar-collapse justify-content-lg-between p-0" id="navbarNav">
+          <ul className="navbar-nav">
             <li className="nav-item" onClick={() => scrollToSection('home')}>
               <a className="nav-link text-white" href="#">Home</a>
             </li>
@@ -49,13 +41,54 @@ function Navbar() {
           </ul>
           <ul className="navbar-nav m-0 p-0">
             <li className="nav-item">
-              <button className="btn btn-secondary m-0 p-0">Administrator</button>
+              <button id="signin-button" onClick={handleSignupClick} className="btn btn-secondary m-0 p-0"> Sign in </button>
             </li>
           </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar;
+const App = () => {
+  const [showSignin, setShowSignin] = useState(false);
+
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const handleSignupClick = () => {
+    setShowSignin(!showSignin);
+  };
+
+  return (
+    <div>
+      <Navbar scrollToSection={scrollToSection} handleSignupClick={handleSignupClick} />
+      {showSignin && (
+        <div id="signin-popup" className="popup signin-background">
+          <div className="signin-form">
+            <h2>Sign in</h2>
+            <form>
+              <label htmlFor="signin-name">Name</label>
+              <input type="text" id="signin-name" placeholder="Name" required />
+              <label htmlFor="signin-email">Email</label>
+              <input type="email" id="signin-email" placeholder="Email" required />
+              <label htmlFor="signin-phone">Phone Number</label>
+              <input type="tel" id="signin-phone" placeholder="Phone Number" required />
+              <div className="complete-signup-button">
+                <button type="submit" className="chrome-yellow-button">
+                  Sign In
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
